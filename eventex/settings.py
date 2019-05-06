2511a77ee7e17abd09a81b53f050c454e7445a23
Flávio Tassan <tassanf@gmail.com>
2019-05-06 14:45:40 -0300

    collectstatic

--- eventex/settings.py
+++ eventex/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # USE_S3 = config('USE_S3', default=False, cast=bool)
 #
